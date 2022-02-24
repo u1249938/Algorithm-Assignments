bc3d1062076c9e31503b62d915e8599cc0b6c6df
u1249938 <u1249938@utah.edu>
2022-02-24 02:22:52 +0000

    fixed a bug relating to string concatination on lines 23, 29, and 33

--- priority_processor_production.py
+++ priority_processor_production.py
@@ -20,17 +20,17 @@ def findSmallest(step, currentLine):
 addLine = findSmallest(0, 0)
 time = list()
 time.append(int(lines[addLine][0]))
-prodLine = "" + addLine
+prodLine = "" + str(addLine)
 i = 1
 for i in range(numSteps):
     smallest = findSmallest(i, addLine)
     if smallest != addLine:
         time.append(int(lines[smallest][i]) + int(switchCosts[i - 1]))
-        prodLine += " " + smallest
+        prodLine += " " + str(smallest)
         addLine = smallest
     else:
         time.append(int(lines[addLine][i]))
-        prodLine += " " + addLine
+        prodLine += " " + str(addLine)
 
 print(time[numSteps - 1])
 print(prodLine)
